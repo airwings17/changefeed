4d77569010238dac0559a50a5e389f9006429a0e
Harsh Saini <harsh.saini@nagarro.com>
2021-12-04 07:27:32 +0530

    first commit

---
+++ changeFeed/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Azure.Cosmos;
+
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace changeFeed
+{
+    class Program
+    {
+
+        public static CosmosClient client { get; set; }
+        static void Main(string[] args)
+        {
+          
+            Task.Run(async () =>
+            {
+                client = new CosmosClient("");
+
+                var database = client.GetDatabase("changefeed");
+
+
+                var container1 = database.GetContainer("lease");
+                var containerData = database.GetContainer("shopping");
+                var cpf = containerData.GetChangeFeedProcessorBuilder<dynamic>("cfpLib-1", Processchange)
+                            .WithLeaseContainer(container1)
+                            .WithInstanceName("instance-1")
+                            //.WithStartTime(DateTime.MinValue.ToUniversalTime())
+                            .Build();
+
+                await cpf.StartAsync();
+                Console.WriteLine("Started changefeed processor --- press any key to stop");
+                Console.ReadKey(true);
+
+            }).Wait();
+
+
+        }
+
+        static async Task Processchange(IReadOnlyCollection<dynamic> changes, CancellationToken cancellationToken)
+        {
+            foreach (var item in changes)
+            {
+                Console.WriteLine($"Document changed {item.id}");
+                Console.Write(item.ToString());
+            }
+        }
+    }
+}
